5d5a677cccf7488a524b36080a4c622a62ebcc4f
making <makingx@gmail.com>
2016-09-13 19:03:23 +0900

    Enable Spring Session

--- src/main/java/mrs/MrsApplication.java
+++ src/main/java/mrs/MrsApplication.java
@@ -2,11 +2,18 @@ package mrs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.session.data.redis.config.ConfigureRedisAction;
 
 @SpringBootApplication
 public class MrsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MrsApplication.class, args);
+	}
+
+	@Bean
+	public static ConfigureRedisAction configureRedisAction() {
+		return ConfigureRedisAction.NO_OP;
 	}
 }

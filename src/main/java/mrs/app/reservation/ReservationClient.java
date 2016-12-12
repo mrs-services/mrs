a0b24cc943395f586106adf26efa2d5dea02c781
making <makingx@gmail.com>
2016-12-12 14:06:06 +0900

    Polish

--- src/main/java/mrs/app/reservation/ReservationClient.java
+++ src/main/java/mrs/app/reservation/ReservationClient.java
@@ -1,15 +1,11 @@
 package mrs.app.reservation;
 
 import org.springframework.hateoas.Resources;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 public interface ReservationClient {
-	Resources<Reservation> findByReservableRoomId(
-			@RequestParam("reservableRoomId") String reservableRoomId);
+	Resources<Reservation> findByReservableRoomId(String reservableRoomId);
 
-	void reserve(@RequestBody Reservation reservation);
+	void reserve(Reservation reservation);
 
-	void cancel(@PathVariable("reservationId") Integer reservationId);
+	void cancel(Integer reservationId);
 }
